4ada9e1f7d592138c13b97640114c36490272db4
Amukozoberit <mwasheb@gmail.com>
2021-08-07 11:50:19 +0300

    added sources instances

---
+++ app/models/sources.py
@@ -0,0 +1,9 @@
+class Sources:
+    def __init__(self,id,name,description,url,country,language):
+        self.id=id,
+        self.name=name,
+        self.description=description
+        self.url=url
+        self.language=language
+        self.country=country
+        
