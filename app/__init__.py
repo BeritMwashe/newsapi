e6501d0a9dca7fe97e5d2a4952301807ccd3f25c
Amukozoberit <mwasheb@gmail.com>
2021-08-07 11:44:57 +0300

    added the urls

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from app.config import DevConfig
 from flask import Flask
 
 
-app=Flask(__name__)
+app=Flask(__name__,instance_relative_config=True)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
