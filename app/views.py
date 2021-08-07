c55395a5aa9de18aafde400cdbebff983f03bdcd
Amukozoberit <mwasheb@gmail.com>
2021-08-07 17:10:32 +0300

    implement headlines and everything endpoitn

--- app/views.py
+++ app/views.py
@@ -8,6 +8,20 @@ from app import app
 @app.route('/')
 def index():
     sources=get_sources()
+   
+    
+    return render_template('index.html',source=sources)
+
+
+@app.route('/articles')
+def articles():
+    
     articles=get_Articles(category='everything',query='food')
+    # return render_template('articles.html',articles=articles)
     return render_template('articles.html',articles=articles)
-    # return render_template('index.html',source=sources)
+@app.route('/top-headlines')
+def headlines():
+   
+    articles=get_Articles(category='top-headlines',query='politics')
+    # return render_template('articles.html',articles=articles)
+    return render_template('articles.html',articles=articles)
