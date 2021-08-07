f5ea3acadd76eab44edaefe5eb53b8141dff38bc
Amukozoberit <mwasheb@gmail.com>
2021-08-07 15:00:20 +0300

    added base page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 
+
 from app.config import DevConfig
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 
 app=Flask(__name__,instance_relative_config=True)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+bootstrap=Bootstrap(app)
 from app import views
